d99397ddc0276b49637bd1c1f9598bf8d032ded3
Vincenzo Lombardi <vincenzolombardi@hotmail.com>
2026-06-03 16:37:43 +0200

    Created new migration for db creation (hardcoded connection string). Closes #3

--- HelpDeskAPI/HelpDeskAPI.Infrastructure/Database/HelpDeskAPIDbContextFactory.cs
+++ HelpDeskAPI/HelpDeskAPI.Infrastructure/Database/HelpDeskAPIDbContextFactory.cs
@@ -8,10 +8,7 @@ public class HelpDeskAPIDbContextFactory : IDesignTimeDbContextFactory<HelpDeskA
     public HelpDeskAPIDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<HelpDeskAPIDbContext>();
-
-        // Hardcode your connection string here JUST for design-time migrations,
-        // or configure it to read from your appsettings.json
-        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TicketingDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=HelpDeskAPIDB;Trusted_Connection=True;MultipleActiveResultSets=true");
 
         return new HelpDeskAPIDbContext(optionsBuilder.Options);
     }
