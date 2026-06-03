fff3bf722b7f065ba031b7796d49952848082154
Vincenzo Lombardi <vincenzolombardi@hotmail.com>
2026-06-03 16:32:00 +0200

    Created basic custom DbContext and factory

---
+++ HelpDeskAPI/HelpDeskAPI.Infrastructure/Database/HelpDeskAPIDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using HelpDeskAPI.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace HelpDeskAPI.Infrastructure.Database;
+public class HelpDeskAPIDbContextFactory : IDesignTimeDbContextFactory<HelpDeskAPIDbContext>
+{
+    public HelpDeskAPIDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<HelpDeskAPIDbContext>();
+
+        // Hardcode your connection string here JUST for design-time migrations,
+        // or configure it to read from your appsettings.json
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TicketingDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+        return new HelpDeskAPIDbContext(optionsBuilder.Options);
+    }
+}
