cd786ee8010be4e1c645e705bc45e81d29eb200c
Vincenzo Lombardi <vincenzolombardi@hotmail.com>
2026-06-03 16:51:44 +0200

    Defined main repos interfaces. Create IAuthService file

---
+++ HelpDeskAPI/HelpDeskAPI.Application/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using HelpDeskAPI.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpDeskAPI.Application.Interfaces;
+
+public interface IAuthService
+{
+    Task<Utente> Login(string );
+}
---
+++ HelpDeskAPI/HelpDeskAPI.Application/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpDeskAPI.Application.Interfaces;
+
+public interface IRepository<T, U>
+{
+    Task Create(T entity);
+    Task<T> GetById(U id);
+    Task<ICollection<T>>GetAll();
+    Task Update(T updatedEntity);
+    Task Delete(T entity);
+}
