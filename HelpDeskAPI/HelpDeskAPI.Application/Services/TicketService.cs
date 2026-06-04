f1946c04ec5b29abf785af7fe739d73bbef7419e
Vincenzo Lombardi <vincenzolombardi@hotmail.com>
2026-06-04 11:49:55 +0200

    Improve DTO, created basic interface for ticket read repo

--- HelpDeskAPI/HelpDeskAPI.Application/Services/TicketService.cs
+++ HelpDeskAPI/HelpDeskAPI.Application/Services/TicketService.cs
@@ -1,9 +1,6 @@
 ﻿using HelpDeskAPI.Application.Interfaces;
 using HelpDeskAPI.Core.DTOs;
 using HelpDeskAPI.Core.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace HelpDeskAPI.Application.Services;
 
---
+++ HelpDeskAPI/HelpDeskAPI.Infrastructure/Repositories/TicketReadRepository.cs
@@ -0,0 +1,23 @@
+﻿using HelpDeskAPI.Application.Interfaces;
+using HelpDeskAPI.Core.DTOs;
+using HelpDeskAPI.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpDeskAPI.Infrastructure.Repositories;
+
+public class TicketReadRepository(HelpDeskAPIDbContext _context) : ITicketReadRepo
+{
+    private readonly HelpDeskAPIDbContext _context = _context;
+    public async Task<ITicketReadRepo> GetActiveTickets()
+    {
+        return await _context.Tickets
+                .AsNoTracking()
+                .Where(t => t.Stato != Core.Models.Stato.CHIUSO)
+                .Select(t => new TicketByStatusResponse(t.Id, t.Titolo, t.Date,t.Stato,t.Utente.Nome ))
+                .ToListAsync();
+
+    }
+}
