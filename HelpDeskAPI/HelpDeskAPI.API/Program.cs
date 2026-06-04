28d3a0bf9891f7e31a0945c3898db11937a6e50f
Vincenzo Lombardi <vincenzolombardi@hotmail.com>
2026-06-04 12:44:28 +0200

    Implemented basic api versioning with health endpoint

--- HelpDeskAPI/HelpDeskAPI.API/Program.cs
+++ HelpDeskAPI/HelpDeskAPI.API/Program.cs
@@ -1,3 +1,4 @@
+using Asp.Versioning;
 using HelpDeskAPI.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,12 +7,17 @@ builder.AddApplicationServices();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
-
+var versionSet = app.NewApiVersionSet()
+    .HasApiVersion(new ApiVersion(1, 0))
+    .ReportApiVersions()
+    .Build();
+var versionedGroup = app.MapGroup("api/v{version:apiVersion}/debug")
+    .WithApiVersionSet(versionSet);
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
+versionedGroup.MapGet("/health", () => { Results.Ok(); });
 app.UseHttpsRedirection();
 
 app.Run();
