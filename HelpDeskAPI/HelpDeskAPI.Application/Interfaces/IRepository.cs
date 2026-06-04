0c259ff646ac73b9efedec635790bd57f54382f2
Vincenzo Lombardi <vincenzolombardi@hotmail.com>
2026-06-04 09:59:35 +0200

    Implemented basic CRUD for Utente, fixed return types in IRepository

--- HelpDeskAPI/HelpDeskAPI.Application/Interfaces/IRepository.cs
+++ HelpDeskAPI/HelpDeskAPI.Application/Interfaces/IRepository.cs
@@ -6,8 +6,8 @@ namespace HelpDeskAPI.Application.Interfaces;
 
 public interface IRepository<T, U>
 {
-    Task Create(T entity);
-    Task<T> GetById(U id);
+    Task<U>Create(T entity);
+    Task<T?> GetById(U id);
     Task<ICollection<T>>GetAll();
     Task Update(T updatedEntity);
     Task Delete(T entity);
---
+++ HelpDeskAPI/HelpDeskAPI.Infrastructure/Repositories/UtenteRepository.cs
@@ -0,0 +1,40 @@
+﻿using HelpDeskAPI.Application.Interfaces;
+using HelpDeskAPI.Core.Models;
+using HelpDeskAPI.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelpDeskAPI.Infrastructure.Repositories;
+
+public class UtenteRepository(HelpDeskAPIDbContext _context) : IUtenteRepository
+{
+    private readonly HelpDeskAPIDbContext _context = _context;
+    public async Task<int> Create(Utente entity)
+    {
+       var e = await _context.Utenti.AddAsync(entity);
+       return e.Entity.Id;
+    }
+
+    public Task Delete(Utente entity)
+    {
+        return Task.FromResult(_context.Utenti.Remove(entity));
+        
+    }
+
+    public async Task<ICollection<Utente>> GetAll()
+    {
+        return await _context.Utenti.ToListAsync();
+    }
+
+    public Task<Utente?> GetById(int id)
+    {
+        return Task.FromResult(_context.Utenti.FirstOrDefault(u => u.Id == id));
+    }
+
+    public Task Update(Utente updatedEntity)
+    {
+        return Task.FromResult(_context.Utenti.Update(updatedEntity));
+    }
+}
