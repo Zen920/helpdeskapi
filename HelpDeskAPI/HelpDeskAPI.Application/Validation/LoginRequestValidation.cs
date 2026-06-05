ace367ac2da80435f981ff5d33e43cf8475fc51e
Vincenzo Lombardi <vincenzolombardi@hotmail.com>
2026-06-05 14:11:43 +0200

    Validation for login request

---
+++ HelpDeskAPI/HelpDeskAPI.Application/Validation/LoginRequestValidation.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using HelpDeskAPI.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace HelpDeskAPI.Application.Validation;
+
+internal partial class LoginRequestValidation : AbstractValidator<LoginRequest>
+{
+    public LoginRequestValidation()
+    {
+        RuleFor(r => r.Email)
+            .NotEmpty()
+            .EmailAddress();
+        RuleFor(r => r.Password)
+            .NotEmpty()
+            .Length(3,20)
+            .Custom((p, context) =>
+            {
+                PasswordRegex().IsMatch(p);
+            });
+    }
+
+    [GeneratedRegex("/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/")]
+    private static partial Regex PasswordRegex();
+}
